e417a89b811fa4c2d69b93882ea8a9d4f158f46c
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2024-01-26 21:30:16 +0530

    Update App.js

--- 03-Review-Corosel/App.js
+++ 03-Review-Corosel/App.js
@@ -98,3 +98,64 @@ prevBtn.addEventListener('click', function(){
 	}
 	showFunction(currentItem);
 })
+
+//updated
+const imageData = [
+	{
+		id: 1,
+		author: 'susan smith',
+		img: 'https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?size=338&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
+		job: 'UX Designer',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, earum?',
+	},
+	{
+		id: 2,
+		author: 'Nick',
+		img: 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
+		job: 'Frontend Developer',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, earum?',
+	},
+	{
+		id: 3,
+		author: 'Lena',
+		img: 'https://img.freepik.com/free-photo/portrait-beautiful-young-woman-with-curly-hair-brown-hat_1142-42780.jpg?size=338&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
+		job: 'UI Designer',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, earum?',
+	},
+	{
+		id: 4,
+		author: 'smith',
+		img: 'https://img.freepik.com/free-psd/3d-illustration-person-with-long-hair_23-2149436197.jpg?size=338&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
+		job: 'Intern',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, earum?',
+	},
+
+	// Add more objects as needed
+];
+
+const personImg = document.querySelector('#person-img');
+const job = document.querySelector('#job');
+const info = document.querySelector('#info');
+const author= document.querySelector('.author');
+
+// selection buttons
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//cuttent item initialy 0 because arrays is 0 based index
+let currentItem = 0;
+
+function showPerson(){
+	const item = imageData
+	personImg.src = imageData.img;
+	job.textContent = imageData.job;
+	info.textContent = imageData.info;
+	author.textContent = imageData.author;
+}
+
+// adding event to html
+//document - refers whole html documnet
+document.addEventListener('DOMContentLoaded', function(){
+	showPerson(currentItem)
+})
