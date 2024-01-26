3b731490b98f23cb72569bf7a5030dd78be26ef2
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2024-01-26 21:58:14 +0530

    Update App.js

--- 03-Review-Corosel/App.js
+++ 03-Review-Corosel/App.js
@@ -9,97 +9,6 @@ textContent
 
 */
 
-// array object
-const reviews = [
-	{
-		id: 1,
-		name: 'Susan smith',
-		job: 'Ux Designer',
-		img: 'https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?size=626&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
-		text: 'ABC',
-	},
-	{
-		id: 2,
-		name: 'Susan smith',
-		job: 'Ux Designer',
-		img: 'https://img.freepik.com/free-photo/portrait-beautiful-young-woman-with-curly-hair-brown-hat_1142-42780.jpg?size=338&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
-		text: 'ABC',
-	},
-	{
-		id: 3,
-		name: 'Susan smith',
-		job: 'Ux Designer',
-		img: 'https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?size=626&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
-		text: 'ABC',
-	},
-	{
-		id: 4,
-		name: 'Susan smith',
-		job: 'Ux Designer',
-		img: 'https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?size=626&ext=jpg&ga=GA1.1.1168400549.1706226034&semt=sph',
-		text: 'ABC',
-	},
-];
-
-// selecting items
-const img = document.querySelector('#person-img');
-const author = document.querySelector('.author');
-const job = document.querySelector('#job');
-const info = document.querySelector('#info');
-
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
-
-
-//starting item must be 0 bcos we are using array based indes
-let currentItem = 0;
-
-//load initial items
-window.addEventListener('DOMContentLoaded', function(){
-  showFunction(currentItem);
-})
-
-function callBackFunc(){
-  
-  // console.log('shke and bake');
-  // //assigned the cuttentItem to a item const variabel
-  // const item = reviews[currentItem];
-  // //adding src to img tag dynamically
-  // img.src = item.img;
-  // author.textContent = item.name;
-  // job.textContent = item.job;
-  // info.textContent = item.text;
-
-  //refactoruing the code
-}
-
-// show person based on item
-function showFunction(person){
-	const item = reviews[person];
-	//adding src to img tag dynamically
-	img.src = item.img;
-	author.textContent = item.name;
-	job.textContent = item.job;
-	info.textContent = item.text;
-}
-
-nextBtn.addEventListener('click', function(){
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showFunction(currentItem);
-})
-prevBtn.addEventListener('click', function(){
-  currentItem--;
-	if (currentItem < 0) {
-		currentItem = reviews.length - 1;
-	}
-	showFunction(currentItem);
-})
-
-//updated
 const imageData = [
 	{
 		id: 1,
@@ -136,7 +45,7 @@ const imageData = [
 const personImg = document.querySelector('#person-img');
 const job = document.querySelector('#job');
 const info = document.querySelector('#info');
-const author= document.querySelector('.author');
+const author = document.querySelector('.author');
 
 // selection buttons
 const prevBtn = document.querySelector('.prev-btn');
@@ -144,10 +53,10 @@ const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
 
 //cuttent item initialy 0 because arrays is 0 based index
+//meaning the first element in an array has an index of 0. So, if currentItem is 0, you are currently pointing to the first element in the array.
 let currentItem = 0;
 
-function showPerson(){
-	const item = imageData
+function showPerson(imageData) {
 	personImg.src = imageData.img;
 	job.textContent = imageData.job;
 	info.textContent = imageData.info;
@@ -155,7 +64,23 @@ function showPerson(){
 }
 
 // adding event to html
-//document - refers whole html documnet
-document.addEventListener('DOMContentLoaded', function(){
-	showPerson(currentItem)
+//document - refers whole html documnet 
+//domcontenet loaded is triggerd 
+document.addEventListener('DOMContentLoaded', function () {
+	//here we are pasiing the image data array with current index of 0
+	showPerson(imageData[currentItem]);
+});
+
+nextBtn.addEventListener('click', function(){
+	currentItem = ( currentItem + 1) % imageData.length
+	showPerson(imageData[currentItem])
 })
+prevBtn.addEventListener('click', function(){
+	currentItem = ( currentItem - 1 + imageData.length) % imageData.length;
+	showPerson(imageData[currentItem])
+})
+
+randomBtn.addEventListener('click', function () {
+	currentItem = Math.floor(Math.random() * imageData.length);
+	showPerson(imageData[currentItem]);
+});
